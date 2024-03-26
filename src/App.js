564e7f5ba57697a54618b35e12a2515efbee42a7
Ashwin <113379433+ashwin2001-dot@users.noreply.github.com>
2024-03-26 18:05:20 +0530

    Tic tac toe initial commit (#1)
    Co-authored-by: ashwin <ashwinachu@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const INITIAL_STATE = Array(9).fill(null);
+
+const App = () => {
+  const [board, setBoard] = useState(INITIAL_STATE);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (calculateWinner(board) || board[index]) {
+      return;
+    }
+    const newBoard = [...board];
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const getStatus = () => {
+    const winner = calculateWinner(board);
+    if (winner) {
+      return `Winner: ${winner}`;
+    } else if (board.every((square) => square)) {
+      return 'Draw';
+    } else {
+      return `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="status">{getStatus()}</div>
+      <div className="board">
+        <div className="row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const calculateWinner = (board) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
+};
+
+export default App;
